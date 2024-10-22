f7aef80dbaef5edb2eb665557379520b5708df81
Zeshan <tui79498@temple.edu>
2024-10-21 23:20:22 -0400

    using pnpm instead

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,19 @@ import fs from 'fs';
 import cors from 'cors';
 import { GPTScript, RunEventType } from '@gptscript-ai/gptscript';
 
-const g = new GPTScript();
+
 
 const app = express();
+
 app.use(cors());
+
+const g = new GPTScript();
 
 app.get('/test', (req, res) =>{
     return res.json('test ok');
 });
 
-app.get('/create-story', (req, res) => {
+app.get('/create-story', async (req, res) => {
     const url = req.query.url;
     const dir = uniqid();
     const path = './stories/'+dir;
@@ -23,18 +26,24 @@ app.get('/create-story', (req, res) => {
         url,
     });
 
-    const run = await gptscript.run('./story.gpt', {
+    const opts = {
         input: `--url ${url} --dir ${dir}`, 
         disableCache: true,
-    })
+    };
+
+    console.log('about to run story.gpt');
+
+    const run = await g.run('./story.gpt', opts);
+
+    console.log('awaiting result');
     run.on(RunEventType.Event, ev =>{
-        if(ev.type == RunEventType.CallFinish && ev.output){
+        //if(ev.type == RunEventType.CallFinish && ev.output){
             console.log(ev.output);
-        }
+        //}
     });
     const result = await run.text();
 
-    return res.json('ok');
+    return res.json(result);
 });
 
 app.listen(7070, () => console.log('Listening on port 7070'));
